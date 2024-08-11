fd21440cf0d7a8ee42d6a29e03ad248663062f6d
rios-natalia <nataliarios@alu.ufc.br>
2024-08-11 08:51:53 -0300

    feat: create html endpoint and schema

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,22 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     response = client.get('/')  # Act
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+
+
+def test_root_html_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange
+    response = client.get('/html')  # Act
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert (
+        response.text
+        == """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
+    )
+    # Assert
